4387d5bfc622278f0fbc113011adc8cb75564179
ejisemm <emmanuel.jiss@ericsson.com>
2024-06-05 12:45:07 +0100

    Finished Add and delete

--- app.py
+++ app.py
@@ -7,15 +7,6 @@ list_of_items = []
 #Adding Items
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/', methods=['GET', 'POST'])
-item = request.form['item']
-list_of_items.append(item)
-return render_template('index.html', items=list_of_items)
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
     if request.method == 'POST':
         item = request.form['item']
         list_of_items.append(item)
@@ -23,15 +14,19 @@ def index():
     return render_template('index.html', items=list_of_items)
 
 #delete items
-@app.route('/delete/<item>'
-if item in list_of_items:
-list_of_items.remove(item)
-return redirect(url_for('index'))
-
 @app.route('/delete/<item>', methods=['POST'])
 def delete_item(item):
     if item in list_of_items:
         list_of_items.remove(item)
     return redirect(url_for('index'))
 
+
+@app.route('/update/<item>', methods=['POST'])
+def update_item(item):
+    if request.method == 'POST':
+        new_item = request.form['new_item']
+        if item in list_of_items:
+            item_index = list_of_items.index(item)
+            list_of_items[item_index] = new_item
+        return redirect(url_for('index'))
 
