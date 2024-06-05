9b0495ae26a53e3dca6314e1837e4221c85dd6a7
ejisemm <emmanuel.jiss@ericsson.com>
2024-06-05 11:57:46 +0100

    Add and delete

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+list_of_items = []
 
-@app.route('/')
+#Adding Items
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/', methods=['GET', 'POST'])
+item = request.form['item']
+list_of_items.append(item)
+return render_template('index.html', items=list_of_items)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        item = request.form['item']
+        list_of_items.append(item)
+        return render_template('index.html', items=list_of_items)
+    return render_template('index.html', items=list_of_items)
+
+#delete items
+@app.route('/delete/<item>'
+if item in list_of_items:
+list_of_items.remove(item)
+return redirect(url_for('index'))
+
+@app.route('/delete/<item>', methods=['POST'])
+def delete_item(item):
+    if item in list_of_items:
+        list_of_items.remove(item)
+    return redirect(url_for('index'))
+
+
